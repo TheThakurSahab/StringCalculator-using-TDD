98a965a91fde44ee5c30f5fe136f5aa42588afd6
Akash Singh <akashsingh.work@outlook.com>
2020-12-24 16:12:53 +0530

    Using Stream instead of Array to handle more that two number

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,13 +1,13 @@
+import java.util.*;
+import java.util.stream.Stream;
+
 public class Calculator {
     public static int Add(String text){
         if(text.isEmpty()){
             return 0;
-        } else if(text.contains(",")){
-            String [] input = text.split(",");
-            return toInt(input[0]) + toInt(input[1]);
-        }
-        else {
-            return toInt(text);
+        } else{
+            Stream<String> numbers = Arrays.stream(text.split(","));
+            return numbers.mapToInt(Integer::parseInt).sum();
         }
     }
     public static int toInt(String input){
