1c53acb538440e815c7627966e0752c660b63ab1
Akash Singh <akashsingh.work@outlook.com>
2020-12-24 16:34:37 +0530

    Adding test case to check the outcome if number are delimited by comma or new line

--- Test/CalculatorTest.java
+++ Test/CalculatorTest.java
@@ -21,14 +21,20 @@ public class CalculatorTest {
         assertEquals(8, Calculator.Add("5,3"));
         assertEquals(10, Calculator.Add("7,3"));
     }
+
     @Test
     public void shouldReturnSumIfStringHaveMultipleNumber(){
         assertEquals(12,Calculator.Add("6,4,2"));
         assertEquals(15,Calculator.Add("6,5,4"));
     }
+
     @Test
-    public void shouldReturnSumIfStringHaveTwoNumberDelimitedBySpecialChar(){
+    public void shouldReturnSumIfStringHaveTwoNumberDelimitedBySpecialChar() {
         assertEquals(11, Calculator.Add("8\n3"));
+    }
+    @Test
+    public void shouldReturnSumIfStringHaveMultipleNumbersDelimitedByCommaOrNewLine(){
+        assertEquals(22, Calculator.Add("5,2,12\n3"));
     }
 
 }
