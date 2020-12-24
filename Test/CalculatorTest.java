e56091b2b3b4ab9ab3a6acdd094ab022f62f49f4
Akash Singh <akashsingh.work@outlook.com>
2020-12-24 16:10:34 +0530

    added more test case to check the coverage

--- Test/CalculatorTest.java
+++ Test/CalculatorTest.java
@@ -13,15 +13,22 @@ public class CalculatorTest {
     @Test
     public void shouldReturnNumberIfStringHaveNumber(){
         assertEquals(1,Calculator.Add("1"));
+        assertEquals(5,Calculator.Add("5"));
     }
 
     @Test
     public void shouldReturnSumIfStringHaveTwoNumberDelimitedByComma(){
         assertEquals(8, Calculator.Add("5,3"));
+        assertEquals(10, Calculator.Add("7,3"));
     }
     @Test
     public void shouldReturnSumIfStringHaveMultipleNumber(){
         assertEquals(12,Calculator.Add("6,4,2"));
+        assertEquals(15,Calculator.Add("6,5,4"));
+    }
+    @Test
+    public void shouldReturnSumIfStringHaveTwoNumberDelimitedBySpecialChar(){
+        assertEquals(10, "5\n5");
     }
 
 }
