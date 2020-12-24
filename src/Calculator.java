914a47b2463b4046af1bb16b61103f6fb730b271
Akash Singh <akashsingh.work@outlook.com>
2020-12-24 11:51:00 +0530

    Improving add method to handle two number seperated by comma

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,11 @@ public class Calculator {
     public static int Add(String text){
         if(text.isEmpty()){
             return 0;
-        } else {
+        } else if(text.contains(",")){
+            String [] input = text.split(",");
+            return Integer.parseInt(input[0]) + Integer.parseInt(input[1]);
+        }
+        else {
             return Integer.parseInt(text);
         }
     }
