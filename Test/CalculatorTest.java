d934cb5c2ebf8a490433c75cfb682390250f0899
Akash Singh <akashsingh.work@outlook.com>
2020-12-24 16:43:50 +0530

    Adding test case to check the outcome if input have custom delimiter

--- Test/CalculatorTest.java
+++ Test/CalculatorTest.java
@@ -36,5 +36,9 @@ public class CalculatorTest {
     public void shouldReturnSumIfStringHaveMultipleNumbersDelimitedByCommaOrNewLine(){
         assertEquals(22, Calculator.Add("5,2,12\n3"));
     }
+    @Test
+    public void shouldReturnSumIfStringHaveCustomDelimiter(){
+        assertEquals(20, Calculator.Add("//;\n5,15"));
+    }
 
 }
